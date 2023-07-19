d645e9f8a9866c92353d8a489a95e27964a4dde2
KrasimirDmtr <kdimitrov13@abv.bg>
2023-07-19 15:35:06 +0300

    Add LoadingPage Component

---
+++ bodyFlex/src/app/loadingPage/loading-page/loading-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-page',
+  templateUrl: './loading-page.component.html',
+  styleUrls: ['./loading-page.component.css']
+})
+export class LoadingPageComponent {
+
+}
