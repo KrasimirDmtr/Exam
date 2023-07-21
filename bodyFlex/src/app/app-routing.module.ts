8922607cb6c32c9908e84846c5fd4e7c4a5330c4
KrasimirDmtr <kdimitrov13@abv.bg>
2023-07-21 18:16:42 +0300

    Create and Add Login Page html and css

--- bodyFlex/src/app/app-routing.module.ts
+++ bodyFlex/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './core/home-page/home-page.component';
 import { LoadingPageComponent } from './loading-page/loading-page-comp/loading-page.component';
 import { SplitComponent } from './split/split/split.component';
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component:LoadingPageComponent },
   {path: 'home', component: HomePageComponent},
   {path: 'splits', component: SplitComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'register', component:RegisterComponent}
 ];
 
 @NgModule({
