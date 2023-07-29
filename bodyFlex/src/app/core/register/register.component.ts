7345f5e36075fb2ca838b97e66deeda691deba66
KrasimirDmtr <kdimitrov13@abv.bg>
2023-07-29 16:14:56 +0300

    Fix and Update Register Form validators and style

--- bodyFlex/src/app/core/register/register.component.ts
+++ bodyFlex/src/app/core/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms'
+import { DEFAULT_DOMAINS } from 'src/app/shared/domains';
+import { emailValidator } from 'src/app/shared/validators/email-validator';
 import { matchPassValidator } from 'src/app/shared/validators/match-pass';
 
 @Component({
@@ -9,7 +11,7 @@ import { matchPassValidator } from 'src/app/shared/validators/match-pass';
 })
 export class RegisterComponent {
   regForm = this.fb.group({
-    email: ['', Validators.required, Validators.email],
+    email: ['', Validators.required, emailValidator(DEFAULT_DOMAINS)],
     passGroup: this.fb.group(
       {
         password: ['', [Validators.required, Validators.minLength(5)]],
